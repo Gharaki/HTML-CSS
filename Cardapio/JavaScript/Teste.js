fd568f45e64be075a2fa37a0bb224aa9576e91b3
Guilherme Haraki <guiharaki@gmail.com>
2025-02-20 19:07:23 -0300

    📜 Cardápio de Restaurante - HTML & CSS | 🍽️
    Este projeto é uma página de cardápio de restaurante desenvolvida com HTML e CSS. Possui um design com o básico que aprendi de CSS. O layout está organizado em seções como lanches porções e bebidas. Com um som de restaurante no fundo.

---
+++ Cardapio/JavaScript/Teste.js
@@ -0,0 +1,9 @@
+var mistery = 1;
+console.log(typeof(mistery));
+
+mistery="1";
+console.log(typeof(mistery));
+
+if(1=="1") consolelog("JS Faz muitas interpretações");
+if (1!=="1") consolelog("use === para estritamente igual e !=== para estritamente diferente")
+    // JS considera tudo como string;
